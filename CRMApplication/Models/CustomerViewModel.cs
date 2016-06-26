bdbe559ddc4f46c504a4eddecd2f0e5ebe8cc4bf
Scott <Scott@DESKTOP-VUPFLSV>
2016-06-26 01:26:04 -0500

    Commit application - Initial push

---
+++ CRMApplication/Models/CustomerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CRMApplication.Models
+{
+    
+    [Table("Customer")]
+    public class Customer
+    {
+        [Key]
+
+        public int CustomerID { get; set; }
+
+        public int AccountID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string emailAddress { get; set; }
+
+        public string CompanyName { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+
+}
---
+++ CRMApplication/Models/NoteViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CRMApplication.Models
+{
+    [Table("Note")]
+    public class NoteViewModel
+    {
+        [Key]
+        public int NoteID { get; set; }
+
+        public int CustomerID { get; set; }
+
+        public string Body { get; set; }
+
+        public string UserCreated { get; set; }
+       
+    }
+}
